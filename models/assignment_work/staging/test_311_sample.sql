a9aff9533996b4799f66a7dabe7e0bb5de3a372b
Heeje Yoo <heeje90@gmail.com>
2026-03-30 09:45:36 +0000

    troubleshooting

--- models/assignment_work/staging/test_311_sample.sql
+++ models/assignment_work/staging/test_311_sample.sql
@@ -1,7 +1,6 @@
--- Quick test to verify source connection works
- SELECT
-     unique_key,
-     created_date,
-     complaint_type,
-     borough
- FROM {{ source('raw', 'source_dot_service_requests_history') }};
+select
+  unique_key,
+  created_date,
+  complaint_type,
+  borough
+from {{ source('raw', 'source_dot_service_requests_history') }}
