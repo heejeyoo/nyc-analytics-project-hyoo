ffb04f00278d7585c4a87eaeab7983eb8227b8bb
Heeje Yoo <heeje90@gmail.com>
2026-03-31 00:01:17 +0000

    editing related to zip code from both fact tables

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -3,7 +3,18 @@
 WITH all_locations AS (
    -- Get locations from 311 requests
    SELECT DISTINCT
-      borough --TODO replace (HINT: look @ dimensional model & staging data!)
+        borough --TODO replace (HINT: look @ dimensional model & staging data!)
+        -- Location - clean zip code, handling several common zip code data problems
+        CASE
+            WHEN UPPER(TRIM(CAST(incident_zip AS STRING))) IN ('N/A', 'NA') THEN NULL
+            WHEN UPPER(TRIM(CAST(incident_zip AS STRING))) = 'ANONYMOUS' THEN 'Anonymous'
+            WHEN LENGTH(CAST(incident_zip AS STRING)) = 5 THEN CAST(incident_zip AS STRING)
+            WHEN LENGTH(CAST(incident_zip AS STRING)) = 9 THEN CAST(incident_zip AS STRING)
+            WHEN LENGTH(CAST(incident_zip AS STRING)) = 10
+                AND REGEXP_CONTAINS(CAST(incident_zip AS STRING), r'^\d{5}-\d{4}')
+            THEN CAST(incident_zip AS STRING)
+            ELSE NULL
+        END AS zip_code
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
 
@@ -12,6 +23,17 @@ WITH all_locations AS (
    -- Get locations from restaurant applications
    SELECT DISTINCT
        borough -- TODO replace (HINT: look @ dimensional model & staging data!)
+       -- Location - clean zip code, handling several common zip code data problems
+       CASE
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) IN ('N/A', 'NA') THEN NULL
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) = 'ANONYMOUS' THEN 'Anonymous'
+           WHEN LENGTH(CAST(zip AS STRING)) = 5 THEN CAST(zip AS STRING)
+           WHEN LENGTH(CAST(zip AS STRING)) = 9 THEN CAST(zip AS STRING)
+           WHEN LENGTH(CAST(zip AS STRING)) = 10
+               AND REGEXP_CONTAINS(CAST(zip AS STRING), r'^\d{5}-\d{4}')
+           THEN CAST(zip AS STRING)
+           ELSE NULL
+       END AS zip_code,
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
 ),
