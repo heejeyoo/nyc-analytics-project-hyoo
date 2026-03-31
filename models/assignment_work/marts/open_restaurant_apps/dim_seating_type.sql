d42afd1d7738911360aa7d9d0cdb5175a71cb551
Heeje Yoo <heeje90@gmail.com>
2026-03-31 02:07:58 +0000

    fixed seating_interest

--- models/assignment_work/marts/open_restaurant_apps/dim_seating_type.sql
+++ models/assignment_work/marts/open_restaurant_apps/dim_seating_type.sql
@@ -1,7 +1,7 @@
 -- Seating type dimension for open restaurant seating applications
 WITH seating_types AS (
    SELECT DISTINCT
-       seating_interest_sidewalk AS seating_interest,
+       seating_interest,
     CASE
         WHEN seating_interest IN ('sidewalk','both','openstreets') then TRUE
         ELSE FALSE
@@ -13,7 +13,7 @@ WITH seating_types AS (
 --TODO: Replace this comment with a CASE WHEN .. statement that handles the different possibilities for approved_for_sidewalk_seating and approved_for_roadway_seating in the data
 --NOTE: The final result we want to select here is two boolean columns (TRUE or FALSE values in them), one column approved_for_sidewalk (TRUE or FALSE value), and one column approved_for_roadway 
    FROM {{ref('stg_nyc_open_restaurant_apps')}} --TODO: reference the appropriate staging table!
-   WHERE seating_interest_sidewalk IS NOT NULL
+   WHERE seating_interest IS NOT NULL
 ),
 seating_dimension AS (
    SELECT
