4067d7c9fcb9318e6d6bbd13be6279bce39899b7
Heeje Yoo <heeje90@gmail.com>
2026-05-01 00:33:15 +0000

    business_address to full_address

--- models/assignment_work/marts/open_restaurant_apps/fact_restaurant_application.sql
+++ models/assignment_work/marts/open_restaurant_apps/fact_restaurant_application.sql
@@ -41,7 +41,7 @@
           -- Data from staging table directly that doesn't come from dimensions follows
 
           -- Location details
-          CAST(r.business_address AS STRING) AS business_address,
+          CAST(r.full_address AS STRING) AS business_address,
           CAST(r.street AS STRING) AS street,
           CAST(r.bulding_number AS STRING) AS building_number,
           CAST(r.latitude AS FLOAT64) AS latitude,
